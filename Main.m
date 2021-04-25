d262d082c1630b607f2e5165ba12096598768af6
RudySagar2020a <61991445+RudySagar2020a@users.noreply.github.com>
2021-04-25 21:09:04 +1000

    tested some code... need t fix

--- Main.m
+++ Main.m
@@ -8,19 +8,18 @@ clf
 
 %% MAIN CLASS FOR EXECUTING CODE
 
-workspace = [-2 2 -2 2 -2 2];
+w = 10;
+workspace = [-w w -w w -w w];
 
 % EDIT ROBOT ARM TRANSLATION HERE---
 
 K1Loc = transl(0.0, 0.0, 0.0);
 
-K1 = ItemTest(workspace,'KiJoint0',1, K1Loc);
+K1 = ItemTest(workspace,'KiJoint0', K1Loc);
 
-KinovaLoc = transl(0.0,0.0,0.1); %TRANSLATE FOR UR3 Arm
+% KinovaLoc = transl(0.0,0.0,0.1); %TRANSLATE FOR UR3 Arm
 %NOTE: POSE for LINEAR UR5 changed @LinearUR5 (line 62)
 
 % 3D PLOT ROBOT ARM IN WORKSPACE---
 
 % KinovaArm = Kinova(workspace, 'KinovaArm', KinovaLoc);
-
-
