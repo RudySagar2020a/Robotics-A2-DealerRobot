8ed6b9c6671c43d6b07103170f8cdfad47c19b9c
balidaemion <68802549+balidaemion@users.noreply.github.com>
2021-05-05 11:32:10 +1000

    Add files via upload

--- Main.m
+++ Main.m
@@ -2,24 +2,36 @@
 
 close all
 set(0,'DefaultFigureWindowStyle','docked')
+view(3);
 clear
 clc
 clf
 
 %% MAIN CLASS FOR EXECUTING CODE
 
-w = 5;
+w = 2;
 workspace = [-w w -w w -w w];
 
-% EDIT ROBOT ARM TRANSLATION HERE---
+%generate & Plot table, arm & card holders
 
-K1Loc = transl(0.0, 0.0, 0.0);
+getTable();
+robot = Kinova;
+CH = getCardHolders;
+cards = getCards;
 
-% K1 = ItemTest(workspace,'KiJoint0', K1Loc);
+%% 
 
-KinovaLoc = transl(0.0,0.0,0.1); %TRANSLATE FOR UR3 Arm
-%NOTE: POSE for LINEAR UR5 changed @LinearUR5 (line 62)
+rotmatrix = [-1,1,0,0;
+            0,-1,1,0;
+            1,0,1,0;
+            0,0,0,1];
+q = zeros(1,6);
+nextq = robot.model.ikcon(cards.card{1}.base*trotx(-pi/2),q)
+%q_ch1 = [62.2 -69 -98 -69.4 -70 0]*pi/180;
+traj = jtraj(q,nextq,50);
+for i=1:50
+    q = traj(i,:);
+    animate(robot.model,q);
+    pause(.05);
+end
 
-% 3D PLOT ROBOT ARM IN WORKSPACE---
-
-KinovaArm = Kinova(workspace, 'KinovaArm', KinovaLoc);
