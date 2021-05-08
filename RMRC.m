513a603413cfac0460acaa2288a0996caa01b38e
RudySagar2020a <Rudra.B.Sagar@student.uts.edu.au>
2021-05-08 19:32:01 +1000

    Added robot transforms and poses

--- RMRC.m
+++ RMRC.m
@@ -2,10 +2,10 @@
 % Lab 9 - Question 1 - Resolved Motion Rate Control in 6DOF referenced to
 % apply towards Kinova Arm movement (end effector position)
 
-function [qMatrix, steps] = RMRC(robot)%robot, startPose, goalPose, Time)
+function [qMatrix, steps] = RMRC(robot, startPose, goalPose)%, Time)
 % 1.1) Set parameters for the simulation
 % mdl_puma560;        % Load robot model
-Kinova = robot.model;
+
 t = 10;             % Total time (s)
 deltaT = 0.1;      % Control frequency (discrete timestep)
 steps = t/deltaT;   % No. of steps for simulation
@@ -25,34 +25,54 @@ positionError = zeros(3,steps); % For plotting trajectory error
 angleError = zeros(3,steps);    % For plotting trajectory error
 
 % 1.3) Set up trajectory, initial pose
-s = lspb(0,1,steps);                     % Trapezoidal trajectory scalar
-for i=1:steps
-    x(1,i) = (1-s(i))*0.35 + s(i)*0.35;  % Points in x
-    x(2,i) = (1-s(i))*-0.55 + s(i)*0.55; % Points in y
-    x(3,i) = 0.5 + 0.2*sin(i*delta);     % Points in z
-    theta(1,i) = 0;                      % Roll angle
-    theta(2,i) = 5*pi/9;                 % Pitch angle
-    theta(3,i) = 0;                      % Yaw angle
+initQ = robot.model.fkine(startPose);                                       % get end effector pose from initial joint positions
+endQ  = goalPose;                                                           % set the goalPose of end effector
+
+iQ = initQ(1:3,4);                                                          % get the xyz values from fkine transform matrix
+eQ = endQ(1:3,4);                                                           % get the xyz values from transform matrix
+
+xscalar = lspb(iQ(1), eQ(1), steps);
+yscalar = lspb(iQ(2), eQ(2), steps);
+zscalar = lspb(iQ(3), eQ(3), steps);
+
+for  i = 1:steps
+     x(1,i) = xscalar;      % Points in x
+     x(2,i) = yscalar;      % Points in y
+     x(3,i) = zscalar;      % Points in z
+     theta(1,i) = 0;        % Roll angle
+     theta(2,i) = 0;        % Pitch angle
+     theta(3,i) = 0;        % Yaw angle
 end
+
+% s = lspb(0,1,steps);                     % Trapezoidal trajectory scalar
+% for i=1:steps
+%     x(1,i) = (1-s(i))*0.35 + s(i)*0.35;  % Points in x
+%     x(2,i) = (1-s(i))*-0.55 + s(i)*0.55; % Points in y
+%     x(3,i) = 0.5 + 0.2*sin(i*delta);     % Points in z
+%     theta(1,i) = 0;                      % Roll angle
+%     theta(2,i) = 5*pi/9;                 % Pitch angle
+%     theta(3,i) = 0;                      % Yaw angle
+% end
 
 T = [rpy2r(theta(1,1),theta(2,1),theta(3,1)) x(:,1);zeros(1,3) 1];          % Create transformation of first point and angle
 q0 = zeros(1,6);                                                            % Initial guess for joint angles
 % qMatrix - will give joint angles to starting robot arm pose
-qMatrix(1,:) = robot.model.ikcon(T,q0);                                            % Solve joint angles to achieve first waypoint
+% qMatrix(1,:) = robot.model.ikcon(T,q0);                                     % Solve joint angles to achieve first waypoint
+qMatrix(1,i) = startPose;
 
 % 1.4) Track the trajectory with RMRC
 for i = 1:steps-1
-    T = robot.model.fkine(qMatrix(i,:));                                           % Get forward transformation at current joint state
+    T = robot.model.fkine(qMatrix(i,:));                                    % Get forward transformation at current joint state
     deltaX = x(:,i+1) - T(1:3,4);                                         	% Get position error from next waypoint
     Rd = rpy2r(theta(1,i+1),theta(2,i+1),theta(3,i+1));                     % Get next RPY angles, convert to rotation matrix
     Ra = T(1:3,1:3);                                                        % Current end-effector rotation matrix
-    Rdot = (1/deltaT)*(Rd - Ra);                                                % Calculate rotation matrix error
+    Rdot = (1/deltaT)*(Rd - Ra);                                            % Calculate rotation matrix error
     S = Rdot*Ra';                                                           % Skew symmetric!
     linear_velocity = (1/deltaT)*deltaX;
     angular_velocity = [S(3,2);S(1,3);S(2,1)];                              % Check the structure of Skew Symmetric matrix!!
     deltaTheta = tr2rpy(Rd*Ra');                                            % Convert rotation matrix to RPY angles
     xdot = W*[linear_velocity;angular_velocity];                          	% Calculate end-effector velocity to reach next waypoint.
-    J = robot.model.jacob0(qMatrix(i,:));                 % Get Jacobian at current joint state
+    J = robot.model.jacob0(qMatrix(i,:));                                   % Get Jacobian at current joint state
     m(i) = sqrt(det(J*J'));
     if m(i) < epsilon  % If manipulability is less than given threshold
         lambda = (1 - m(i)/epsilon)*5E-2;
@@ -60,15 +80,15 @@ for i = 1:steps-1
         lambda = 0;
     end
     invJ = inv(J'*J + lambda *eye(6))*J';                                   % DLS Inverse
-    qdot(i,:) = (invJ*xdot)';                                                % Solve the RMRC equation (you may need to transpose the         vector)
+    qdot(i,:) = (invJ*xdot)';                                               % Solve the RMRC equation (you may need to transpose the         vector)
     for j = 1:6                                                             % Loop through joints 1 to 6
-        if qMatrix(i,j) + deltaT*qdot(i,j) < robot.model.qlim(j,1)                     % If next joint angle is lower than joint limit...
+        if qMatrix(i,j) + deltaT*qdot(i,j) < robot.model.qlim(j,1)          % If next joint angle is lower than joint limit...
             qdot(i,j) = 0; % Stop the motor
-        elseif qMatrix(i,j) + deltaT*qdot(i,j) > robot.model.qlim(j,2)                 % If next joint angle is greater than joint limit ...
+        elseif qMatrix(i,j) + deltaT*qdot(i,j) > robot.model.qlim(j,2)      % If next joint angle is greater than joint limit ...
             qdot(i,j) = 0; % Stop the motor
         end
     end
-    qMatrix(i+1,:) = qMatrix(i,:) + deltaT*qdot(i,:);                         	% Update next joint state based on joint velocities
+    qMatrix(i+1,:) = qMatrix(i,:) + deltaT*qdot(i,:);                       % Update next joint state based on joint velocities
     positionError(:,i) = x(:,i+1) - T(1:3,4);                               % For plotting
     angleError(:,i) = deltaTheta;                                           % For plotting
 end
