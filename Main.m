32a05a4d5e781f4aef75f7d2f4d7cee19424462f
RudySagar2020a <Rudra.B.Sagar@student.uts.edu.au>
2021-05-08 19:29:57 +1000

    Added tests for RMRC cards movement (still failing)

--- Main.m
+++ Main.m
@@ -16,22 +16,29 @@ workspace = [-w w -w w -w w];
 
 getTable();
 robot = Kinova;
-CH = getCardHolders;
+CH    = getCardHolders;
 cards = getCards;
+
+%% Testing RMRC movement
+
+moveCards(Kinova, cards.getpos*transl(-0.4,0,1)*trotz(pi/2));
+iQ = Kinova.GetPose;
+Pose = iQ(1:3,4);
+[qMatrix, steps] = moveDeal(Kinova, transl(Pose)*transl(0,0,0.9), 1.5);
 
 %% 
 
-rotmatrix = [-1,1,0,0;
-            0,-1,1,0;
-            1,0,1,0;
-            0,0,0,1];
-q = zeros(1,6);
-nextq = robot.model.ikcon(cards.card{1}.base*trotx(-pi/2),q)
-%q_ch1 = [62.2 -69 -98 -69.4 -70 0]*pi/180;
-traj = jtraj(q,nextq,50);
-for i=1:50
-    q = traj(i,:);
-    animate(robot.model,q);
-    pause(.05);
-end
+% rotmatrix = [-1,1,0,0;
+%             0,-1,1,0;
+%             1,0,1,0;
+%             0,0,0,1];
+% q = zeros(1,6);
+% nextq = robot.model.ikcon(cards.card{1}.base*trotx(-pi/2),q)
+% %q_ch1 = [62.2 -69 -98 -69.4 -70 0]*pi/180;
+% traj = jtraj(q,nextq,101);
+% for i=1:101
+%     q = traj(i,:);
+%     animate(robot.model,q);
+%     pause(.05);
+% end
 
